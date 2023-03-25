f842dfe26ad56df51b139f0c38c98b752461905b
Abdullah Al Mahmud <48104263+zero-abd@users.noreply.github.com>
2023-03-25 15:37:22 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['note'];
 $formcontent="From: $name \nMessage: $message";
-$recipient = "ahmedfara66067@gmail.com";
+$recipient = "ahmedfarhan66067@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email\r\n";
 mail($recipient, $subject, $formcontent, $mailheader);
